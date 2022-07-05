8cfd630ea1fe663034f56c5f463a741637ec0bdc
Jean <jeanvictorgithubprojects@gmail.com>
2022-07-05 19:07:12 -0300

    feat: Get Lessons with GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
 
-//Para fazer querys graphQL com a sintaxe highlight
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher{
-        name
-      }
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title:string;
-  teacher: {
-    name:string
-  }
-}
+
 function App() {
-  const { data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
 
   return (
-   <ul>
-      {
-        data?.lessons.map( lesson =>(
-          <li key={lesson.id}>{lesson.title} - {lesson.teacher.name}</li>
-        ))
-      }
-   </ul>
+    <div>
+      <Event />
+<h1>Hello world</h1>
+    </div>
   )
 }
 
